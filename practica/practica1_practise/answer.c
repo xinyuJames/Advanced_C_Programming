81e336b828bf37296f823450bf40df145e61a04b
Xinyu Liu <liuxinyujames@gmail.com>
2025-03-08 18:26:36 -0500

    add practica 1 practise

---
+++ practica/practica1_practise/answer.c
@@ -0,0 +1,11 @@
+int get_section() {
+    // make this function return your section number
+    // Your section should be:
+    // Section 2: Meets TR from 9-10:15am with Prof. Lu
+    // Section 3: Meets TR from 3-4:15pm with Prof. Kulkarni
+    // Section 4: Meets MWF from 2:30-3:20pm with Prof. Gupta
+    
+    // if you wish to see what compiler errors will look like during the practica, make this function have a compiler error
+    // if you want to see what failing tests looks like, enter the wrong number; it will be similar during the practica
+    return 0;
+}
---
+++ practica/practica1_practise/answer.h
@@ -0,0 +1,6 @@
+#ifndef __ANSWER_H__
+#define __ANSWER_H__
+
+int get_section();
+
+#endif
